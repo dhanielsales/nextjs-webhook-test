014b4248f83992224db2e4fa7ce60d0206eb4417
Dhaniel Sales <dhanielr94@gmail.com>
2023-03-06 14:44:23 -0300

    add new routes

--- src/pages/api/body.ts
+++ src/pages/api/body.ts
@@ -8,10 +8,7 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  console.log('url', JSON.stringify(req.url))
-  console.log('cookies', JSON.stringify(req.cookies))
-  console.log('method', JSON.stringify(req.method))
-  console.log('query', JSON.stringify(req.query))
+  console.log('body', JSON.stringify(req.body))
 
   res.status(200).json({ message: 'ok' })
 }
